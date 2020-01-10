f235abfd1f2195e6e9b24929c565ce22a4e6a7dd
Alexandre Laranjeiras <laranja22@hotmail.com>
2020-01-10 00:49:46 -0300

    FiscalCoreWebPrint

---
+++ FiscalCore.WebPrint/Controllers/PdfController.cs
@@ -0,0 +1,93 @@
+﻿using FiscalCore.WebPrint.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using ZionDanfe;
+
+namespace FiscalCore.WebPrint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfController : ControllerBase
+    {
+        [HttpPost]
+        [Route("GerarDanfe")]
+        public async Task<IActionResult> GerarDanfe(List<IFormFile> files)
+        {
+            try
+            {
+                var result = new List<FileUploadResult>();
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        using (var outputStream = new MemoryStream())
+                        {
+                            await file.CopyToAsync(outputStream);
+                            var bytes = outputStream.ToArray();
+                            var xmlText = Encoding.UTF8.GetString(bytes);
+                            
+                            var modelo = ZionDanfe.Modelo.DanfeViewModelCreator.CriarDeStringXml(xmlText);
+                            var pdfStream = new MemoryStream();
+
+                            using (var danfe = new Danfe(modelo))
+                            {
+                                danfe.ViewModel.DefinirTextoCreditos("Desenvolvido por www.laranjeiras.dev (zideun@outlook.com)");
+                                danfe.Gerar();
+                                var bytesPdf = danfe.ObterPdfBytes(pdfStream);
+                                return File(bytesPdf, "Application/pdf", $"{modelo.ChaveAcesso}.pdf");
+                            }
+
+                            
+                            //result.Add(new FileUploadResult() { Name = file.FileName, Length = file.Length, Success = true, Bytes = bytesPdf.ToArray() });
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        result.Add(new FileUploadResult() { Name = file.FileName, Length = file.Length, Success = false, Message = ex.Message });
+                    }
+                }
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("Teste")]
+        public void Teste() {
+
+            var client = new MongoClient(
+                "mongodb+srv://zideunerp3:$Aa78222912@zideunerp-hviht.azure.mongodb.net/test?retryWrites=true&w=majority"
+            );
+            var database = client.GetDatabase("test");
+            IMongoCollection<dynamic> colNews = database.GetCollection<dynamic>("news");
+            colNews.InsertOne(new { nome = "Alex", sobrenome = "Laranjeiras" });
+
+
+
+        }
+
+        public byte[] FileToByte(string filename) 
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            var ImageData = StreamToByte(fs);
+            fs.Close();
+            return ImageData; //return the byte data
+        }
+
+        public byte[] StreamToByte(Stream stream)
+        {
+            byte[] ImageData = new byte[stream.Length];
+            stream.Read(ImageData, 0, System.Convert.ToInt32(stream.Length));
+            return ImageData;
+        }
+    }
+}
---
+++ FiscalCore.WebPrint/Models/FileUploadResult.cs
@@ -0,0 +1,11 @@
+﻿namespace FiscalCore.WebPrint.Models
+{
+    public class FileUploadResult
+    {
+        public long Length { get; set; }
+        public string Name { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public byte[] Bytes { get; set; }
+    }
+}
