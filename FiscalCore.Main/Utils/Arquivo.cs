976b23c929f3a991210bb19a0df9a64b0de9360c
Alexandre Laranjeiras <laranja22@hotmail.com>
2020-08-08 00:58:15 -0300

    Atualizado para dotnetcore 3.1 Adicionado funções de arquivos ao Utils.

--- FiscalCore.Main/Utils/Arquivo.cs
+++ FiscalCore.Main/Utils/Arquivo.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using FiscalCore.Main.Configuracoes;
+using System;
 using System.IO;
+using System.Text;
 
 namespace FiscalCore.Main.Utils
 {
     public static class Arquivo
     {
+        public static void SalvarArquivoAutorizado(DFeBR.EmissorNFe.Dominio.NotaFiscalEletronica.NFe nfe, IConfiguracaoServico _configServico, string conteudo)
+        {
+            var nomeArquivo = $"{nfe.infNFe.Id}.xml";
+            var ambiente = nfe.infNFe.ide.tpAmb.ToString();
+            var dataFiscal = nfe.infNFe.ide.dhEmi;
+
+            SalvarArquivoXml(_configServico.DiretorioSalvarXml, ambiente, "Autorizacao", dataFiscal, nomeArquivo ,conteudo);
+        }
+
+        public static void SalvarArquivoXml(string diretorioBase, string ambiente, string subDiretorio, DateTimeOffset dataFiscal, string nomeArquivo, string conteudo)
+        {
+            if (diretorioBase == null)
+                throw new ArgumentNullException(nameof(diretorioBase), "O caminho do arquivo deve ser informado");
+            if (ambiente == null)
+                throw new ArgumentNullException(nameof(ambiente), "O ambiente do arquivo deve ser informado");
+            if (subDiretorio == null)
+                throw new ArgumentNullException(nameof(ambiente), "O subdiretorio do arquivo deve ser informado");
+            if (nomeArquivo == null)
+                throw new ArgumentNullException(nameof(nomeArquivo), "O nome do arquivo deve ser informado");
+            if (conteudo == null)
+                throw new ArgumentNullException(nameof(conteudo), "O conteúdo do arquivo deve ser informado");
+
+            var diretorio = Path.Combine(diretorioBase, ambiente, dataFiscal.ToString("MM_yyyy"), subDiretorio);
+
+            CriarDiretorioSeNaoExistir(diretorio);
+
+            var encodedText = Encoding.UTF8.GetBytes(conteudo);
+            var c1 = Path.Combine(diretorio, nomeArquivo);
+            using (var sourceStream = new FileStream(c1, FileMode.Append, FileAccess.Write, FileShare.None, 4096, true))
+            {
+                sourceStream.Write(encodedText, 0, encodedText.Length);
+                sourceStream.Close();
+            }
+        }
+
+        /// <summary>
+        ///     Verificar se o diretorio existe
+        /// </summary>
+        /// <param name="caminho">caminho do arquivo</param>
+        /// <returns></returns>
+        public static bool ExisteDiretorio(string caminho)
+        {
+            if (Directory.Exists(caminho))
+                return true;
+            return false;
+        }
+
         public static string CriarDiretorioNaRaizDoApp(string nomeDiretorio)
         {
             var caminho = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, nomeDiretorio);
