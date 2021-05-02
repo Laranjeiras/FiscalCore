142cfb72470f7a6a59956c379d39f86cd184bf1a
Alexandre Laranjeiras <laranja22@hotmail.com>
2021-05-02 01:22:26 -0300

    Adicionado novos tipos

---
+++ FiscalCore/Tipos/NaturezaOperacao.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace FiscalCore.Tipos
+{
+    public enum eNaturezaOperacao
+    {
+        [Description("Venda")]
+        [XmlEnum("Venda")]
+        Venda = 1,
+        [Description("Compra")]
+        [XmlEnum("Compra")]
+        Compra = 2,
+        [Description("Remessa para Venda Fora do Estabelecimento")]
+        [XmlEnum("Remessa para Venda Fora do Estabelecimento")]
+        RemessaParaVendaForaEstabelecimento = 3,
+        [Description("Retorno de Remessa para Venda Fora do Estabelecimento")]
+        [XmlEnum("Retorno de Remessa para Venda Fora do Estabelecimento")]
+        RetornodeRemessaParaVendaForaEstabelecimento = 4,
+        [Description("Devolução")]
+        [XmlEnum("Devolução")]
+        Devolucao = 5,
+        [Description("Retorno")]
+        [XmlEnum("Retorno")]
+        Retorno = 6,
+        [Description("Simples Remessa")]
+        [XmlEnum("Simples Remessa")]
+        SimplesRemessa = 7
+    }
+}
---
+++ FiscalCore/Tipos/TipoNF.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace FiscalCore.Tipos
+{
+    /// <summary>
+    ///     Tipo do Documento Fiscal (0 - entrada; 1 - saída)
+    /// </summary>
+    public enum eTipoNF
+    {
+        [Description("Entrada")]
+        [XmlEnum("0")]
+        Entrada = 0,
+
+        [Description("Saída")]
+        [XmlEnum("1")]
+        Saida = 1
+    }
+}
