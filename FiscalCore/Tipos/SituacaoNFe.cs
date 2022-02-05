b528383f2c7efe1beb752aa9ae17ea03a1c23bde
Alexandre Laranjeiras <laranja22@hotmail.com>
2022-02-05 10:45:59 -0300

    Adicionado tipo Situação NFe

---
+++ FiscalCore/Tipos/SituacaoNFe.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace FiscalCore.Tipos
+{
+    public enum eSituacaoNFe
+    {
+        [Description("Uso Autorizado")]
+        UsoAutorizado = 1,
+        [Description("Uso Denegado")]
+        UsoDenegado = 3,
+        [Description("NFe Cancelada")]
+        NFeCancelada = 3,
+    }
+}
